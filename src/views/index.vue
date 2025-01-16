abdeb19352312ee066c9ab4d8cd7d48e5bbd2bf3
lyh-night <857710208@qq.com>
2025-01-16 10:24:00 +0800

    common(store): 增加修改store方法

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,12 +1,22 @@
 <template>
   <LazyLoading> 首页 </LazyLoading>
   <div />
+  <button @click="updateToken">点击</button>
 </template>
 
 <script setup>
+import { useSystemStore } from '@/stores/system'
+
+const systemStore = useSystemStore()
 onMounted(() => {
+  console.log(systemStore.token, 'token')
   console.log(import.meta.env, '环境模式')
 })
+
+function updateToken() {
+  systemStore.setState([{ attr: 'token', val: '456' }])
+  console.log(systemStore.token, 'token')
+}
 </script>
 
 <style lang="scss" scoped></style>
